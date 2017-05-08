a5b9761d43c3452446a92392dc7a4ab69d38ba90
Alweezy <alvinmutisya@gmail.com>
2017-05-09 00:10:10 +0300

    Catch error on non integer inputs

--- prime_numbers.py
+++ prime_numbers.py
@@ -9,9 +9,12 @@ def is_prime(number):
 
 def collect_prime_numbers(number):
     prime_numbers = []
-    if number >= 2:
-        prime_numbers.append(2)
-    for i in range(3, number + 1):
-        if is_prime(i):
-            prime_numbers.append(i)
-    return prime_numbers
+    if isinstance(number, int):
+        if number >= 2:
+            prime_numbers.append(2)
+        for i in range(3, number + 1):
+            if is_prime(i):
+                prime_numbers.append(i)
+        return prime_numbers
+    else:
+        return 'Only integers allowed'
